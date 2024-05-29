138ac344c33e71e24cf731355335cba404ab7fb6
kiv1n <aloha@kiv1n.ru>
2024-05-29 17:54:02 +0300

    Implement request token contract without oracle

---
+++ contracts/contracts/RequestToken.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @notice A contract that stores request tokens.
+ */
+contract RequestToken is ERC721URIStorage, Ownable {
+    struct Content {
+        address recipient;
+        string task;
+        uint paymentAmount;
+        address paymentToken;
+        uint createdDate;
+        uint acceptDate;
+        uint completeDate;
+        string completeImageURI;
+        bool completeSuccessful;
+    }
+
+    event OracleAddressUpdated(address indexed newOracleAddress);
+
+    uint public nextTokenId;
+    address public oracleAddress;
+    mapping(uint tokenId => Content content) public contents;
+    mapping(address recipient => uint successes) public recipientSuccesses;
+    mapping(address recipient => uint fails) public recipientFails;
+
+    constructor(
+        address initOracleAddress
+    ) ERC721("Request Token", "RQT") Ownable(msg.sender) {
+        oracleAddress = initOracleAddress;
+    }
+
+    function setOracleAddress(address newOracleAddress) public onlyOwner {
+        oracleAddress = newOracleAddress;
+        emit OracleAddressUpdated(newOracleAddress);
+    }
+
+    function create(
+        address recipient,
+        string memory task,
+        uint paymentAmount,
+        address paymentToken,
+        string memory uri
+    ) public {
+        uint256 tokenId = nextTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        Content memory content;
+        content.recipient = recipient;
+        content.task = task;
+        content.paymentAmount = paymentAmount;
+        content.paymentToken = paymentToken;
+        content.createdDate = block.timestamp;
+        contents[tokenId] = content;
+    }
+
+    function accept(uint tokenId) public {
+        // Checks
+        require(contents[tokenId].recipient == msg.sender, "Not recipient");
+        require(contents[tokenId].acceptDate == 0, "Already accepted");
+        // Send tokens to contract
+        require(
+            IERC20(contents[tokenId].paymentToken).transferFrom(
+                _ownerOf(tokenId),
+                address(this),
+                contents[tokenId].paymentAmount
+            ),
+            "Failed to transfer payment to contract"
+        );
+        // Update content
+        contents[tokenId].acceptDate = block.timestamp;
+    }
+
+    function complete(uint tokenId, string memory completeImageURI) public {
+        // Checks
+        require(contents[tokenId].recipient == msg.sender, "Not recipient");
+        require(contents[tokenId].acceptDate != 0, "Not accepted");
+        require(contents[tokenId].completeDate == 0, "Already completed");
+        // Complete without oracle
+        if (oracleAddress == address(0)) {
+            contents[tokenId].completeDate = block.timestamp;
+            contents[tokenId].completeImageURI = completeImageURI;
+            contents[tokenId].completeSuccessful = true;
+            require(
+                IERC20(contents[tokenId].paymentToken).transfer(
+                    contents[tokenId].recipient,
+                    contents[tokenId].paymentAmount
+                ),
+                "Failed to transfer payment to recipient"
+            );
+            recipientSuccesses[contents[tokenId].recipient]++;
+        }
+        // Complete with oracle
+        else {
+            // TODO: Implement
+        }
+    }
+}
---
+++ contracts/test/RequestToken.ts
@@ -0,0 +1,78 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("RequestToken", function () {
+  async function initFixture() {
+    // Get signers
+    const [deployer, userOne, userTwo, userThree] = await ethers.getSigners();
+    // Deploy contracts
+    const usdTokenContractFactory = await ethers.getContractFactory("USDToken");
+    const usdTokenContract = await usdTokenContractFactory.deploy();
+    const requestTokenContractFactory = await ethers.getContractFactory(
+      "RequestToken"
+    );
+    const requestTokenContract = await requestTokenContractFactory.deploy(
+      ethers.ZeroAddress
+    );
+    // Send usd tokens to users
+    await usdTokenContract
+      .connect(deployer)
+      .transfer(userOne, ethers.parseEther("1000"));
+    await usdTokenContract
+      .connect(deployer)
+      .transfer(userTwo, ethers.parseEther("1000"));
+    return {
+      deployer,
+      userOne,
+      userTwo,
+      userThree,
+      usdTokenContract,
+      requestTokenContract,
+    };
+  }
+
+  it("Should support the main flow", async function () {
+    const { userOne, userTwo, usdTokenContract, requestTokenContract } =
+      await loadFixture(initFixture);
+    // Create request
+    await expect(
+      usdTokenContract
+        .connect(userOne)
+        .approve(requestTokenContract.getAddress(), ethers.MaxUint256)
+    ).to.be.not.reverted;
+    await expect(
+      requestTokenContract
+        .connect(userOne)
+        .create(
+          userTwo,
+          "task_1",
+          ethers.parseEther("42"),
+          usdTokenContract,
+          "ipfs://1"
+        )
+    ).to.be.not.reverted;
+    const tokenId = (await requestTokenContract.nextTokenId()) - 1n;
+    // Accept request
+    await expect(
+      requestTokenContract.connect(userTwo).accept(tokenId)
+    ).to.changeTokenBalances(
+      usdTokenContract,
+      [userOne, requestTokenContract],
+      [ethers.parseEther("-42"), ethers.parseEther("42")]
+    );
+    // Complete request
+    await expect(
+      requestTokenContract.connect(userTwo).complete(tokenId, "ipfs://2")
+    ).to.changeTokenBalances(
+      usdTokenContract,
+      [userTwo, requestTokenContract],
+      [ethers.parseEther("42"), ethers.parseEther("-42")]
+    );
+    // Check stats
+    expect(await requestTokenContract.recipientSuccesses(userTwo)).to.be.equal(
+      "1"
+    );
+    expect(await requestTokenContract.recipientFails(userTwo)).to.be.equal("0");
+  });
+});
